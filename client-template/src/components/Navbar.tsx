2117acd98f47e21774557da6cbcd87eec770c475
minhtrifit <minhtri.fit@gmail.com>
2023-10-29 21:17:57 +0700

    update main screen

--- client-template/src/components/Navbar.tsx
+++ client-template/src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import { SlDiamond } from "react-icons/sl";
 import { BsFillBellFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [noti, setNoti] = useState<number>(2);
@@ -10,10 +11,13 @@ const Navbar = () => {
   return (
     <div className="bg-white h-[150px] flex justify-between rounded-md">
       <div className="w-[200px] flex justify-center items-center gap-5 text-[#FF8DD8]"></div>
-      <div className="w-[300px] flex gap-3 justify-center items-center md:gap-5 text-[#FF8DD8] hover:cursor-pointer">
+      <Link
+        to="/"
+        className="w-[300px] flex gap-3 justify-center items-center md:gap-5 text-[#FF8DD8] hover:cursor-pointer"
+      >
         <SlDiamond className="text-2xl md:text-5xl" />
         <p className="text-xl md:text-4xl font-black">DIAMOND</p>
-      </div>
+      </Link>
       <div className="w-[200px] flex justify-center items-center gap-5 text-[#FF8DD8]">
         <div
           onClick={() => {
